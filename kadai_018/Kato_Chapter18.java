c2a2433474ac7386dd7b8d0b80e37b7d76bc4b00
Sakurai-Taiki <bass.taiki0707@gmail.com>
2024-09-29 16:45:23 +0900

    kadai18

--- kadai_018/Kato_Chapter18.java
+++ kadai_018/Kato_Chapter18.java
@@ -14,6 +14,8 @@ abstract public class Kato_Chapter18 {
 	public void commonIntroduce() {
 		familyName = "加藤";
 		address = "東京都中野区〇×";
+		System.out.println("名前は" + familyName + "" + givenName + "です");
+		System.out.println("住所は" + address);
 	}
 	
 	//個別の紹介を出力する
@@ -22,8 +24,6 @@ abstract public class Kato_Chapter18 {
 	
 	//紹介を実行する
 	public void execIntroduce() {
-		System.out.println("名前は" + familyName + givenName + "です");
-		System.out.println("住所は" + address + "です");
 		eachIntroduce();
 		
 	}
